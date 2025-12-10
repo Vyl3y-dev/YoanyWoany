1b0c618162f284a8cc1c1a6037f214751c5e8c38
Vy <vcmunos@proton.me>
2025-12-09 19:43:05 -0500

    added assets, moved funcs to separate window file

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
+	"YoanyWoany/windows"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
-	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
@@ -14,37 +13,8 @@ func main() {
 	mainWindow := a.NewWindow("YOANYWOANY")
 	mainWindow.Resize(fyne.NewSize(1280, 768))
 
-	currentContent := OpenPortal(mainWindow)
-
-	mainWindow.SetContent(currentContent)
+	mainWindow.SetContent(windows.DisplayContent(mainWindow))
 
 	mainWindow.ShowAndRun()
 
-}
-
-func OpenPortal(w fyne.Window) *fyne.Container {
-
-	newLabel := widget.NewLabel("Press to open the portal")
-	newLabel.Move(fyne.NewPos(500, 200))
-	newLabel.Resize(fyne.NewSize(200, 100))
-	portalButton := widget.NewButton("Open Portal", func() { dialog.ShowInformation("Portal Status", "A Portal Has Opened", w) })
-	portalButton.Move(fyne.NewPos(500, 300))
-	portalButton.Resize(fyne.NewSize(200, 100))
-
-	forbiddenFactLabel := widget.NewLabel("You wanna know a forbidden Fact?")
-	forbiddenFactLabel.Move(fyne.NewPos(500, 400))
-	forbiddenFactLabel.Resize(fyne.NewSize(200, 100))
-
-	forbiddenFacts := widget.NewButton("Forbidden Fact", func() { forbiddenFactLabel.SetText("Cats know the secrets of man!") })
-	forbiddenFacts.Move(fyne.NewPos(500, 500))
-	forbiddenFacts.Resize(fyne.NewSize(200, 100))
-
-	displayContent := container.NewWithoutLayout(
-		newLabel,
-		portalButton,
-		forbiddenFactLabel,
-		forbiddenFacts,
-	)
-
-	return displayContent
 }
---
+++ windows/mainWindow.go
@@ -0,0 +1,86 @@
+package windows
+
+import (
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
+)
+
+func DisplayContent(w fyne.Window) *fyne.Container {
+
+	displayContent := container.NewWithoutLayout(
+		OpenPortal(w),
+		HiddenFacts(),
+		FloatingMug(),
+	)
+	return displayContent
+}
+
+func OpenPortal(w fyne.Window) *fyne.Container {
+
+	newLabel := widget.NewLabel("Press to open the portal")
+	newLabel.Move(fyne.NewPos(700, 200))
+	newLabel.Resize(fyne.NewSize(200, 100))
+	portalButton := widget.NewButton("Open Portal", func() { dialog.ShowInformation("Portal Status", "A Portal Has Opened", w) })
+	portalButton.Move(fyne.NewPos(700, 300))
+	portalButton.Resize(fyne.NewSize(200, 100))
+
+	displayContent := container.NewWithoutLayout(
+		newLabel,
+		portalButton,
+	)
+
+	return displayContent
+}
+
+func HiddenFacts() *fyne.Container {
+	forbiddenFactLabel := widget.NewLabel("You wanna know a forbidden Fact?")
+	forbiddenFactLabel.Move(fyne.NewPos(700, 400))
+	forbiddenFactLabel.Resize(fyne.NewSize(200, 100))
+
+	forbiddenFacts := widget.NewButton("Forbidden Fact", func() { forbiddenFactLabel.SetText("Cats know the secrets of man!") })
+	forbiddenFacts.Move(fyne.NewPos(700, 500))
+	forbiddenFacts.Resize(fyne.NewSize(200, 100))
+
+	displayContent := container.NewWithoutLayout(
+		forbiddenFactLabel,
+		forbiddenFacts,
+	)
+
+	return displayContent
+}
+
+func FloatingMug() fyne.CanvasObject {
+	img := canvas.NewImageFromFile("assets/YoanyMug.png")
+	img.FillMode = canvas.ImageFillOriginal
+
+	size := float32(512) // 20–60 px
+	x := float32(100)
+	y := float32(100)
+	speedY := float32(2)
+
+	img.Resize(fyne.NewSize(size, size))
+	img.Move(fyne.NewPos(x, y))
+
+	ticker := time.NewTicker(time.Millisecond * 16)
+	go func() {
+		for range ticker.C {
+			y += speedY
+			if y >= 200 || y <= 0 {
+				speedY = -speedY
+			}
+
+			fyne.Do(func() {
+				img.Move(fyne.NewPos(x, y))
+				img.FillMode = canvas.ImageFillOriginal
+				img.Refresh()
+			})
+		}
+	}()
+
+	return img
+}
